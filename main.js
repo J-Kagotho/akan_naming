ca269a2908baa1d9db6289292d5b45f123e4901a
J-Kagotho <joycekagotho2002@gmail.com>
2021-12-12 19:37:01 -0500

    add javascript

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-var dateNumber = prompt("Enter the date");
-T
+let maleAkanNames = [
+    "kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame",
+];
+let femaleAkanNames = [
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+];
+let daysOfTheWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+if (dateOfMonth < 1 or >31{
+    console.log("Invalid entry!");
+}else{
+    console.log("Give Akan name");
+}
